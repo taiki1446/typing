f347f99f97ff854708c4c1a81c96ea3bbd37d3c8
taiki1446 <kbkr0921@gmail.com>
2020-01-02 23:47:14 +0900

    文章編集機能、ユーザー管理devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: "users/sessions",
+  }
   root to: "games#index"
   
   resources :sentences
